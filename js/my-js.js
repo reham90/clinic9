19aabeccef24a59be36a6eddcdfb3ac4905aada2
reham90 <rehamadel344@gmail.com>
2021-07-04 12:24:55 +0300

    edit

--- js/my-js.js
+++ js/my-js.js
@@ -175,7 +175,7 @@ $(document).ready(function(){
                     items: 2
                   },
                   600: {
-                    items:2
+                    items:3
                   },
                    1200: {
                     items: 5
